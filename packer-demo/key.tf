c130020ac25b1c151f054a294a12b1356db5faf4
sammycoderu <65834178+sammycoderu@users.noreply.github.com>
2020-11-01 20:34:41 -0500

    Update key.tf

--- packer-demo/key.tf
+++ packer-demo/key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
+resource "aws_key_pair" "terraformskeypair" {
+  key_name   = "terraformskeypair"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
