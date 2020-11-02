157871d57d266ad1fb71b351ecc7f234a468d2f9
sammycoderu <65834178+sammycoderu@users.noreply.github.com>
2020-11-02 16:38:52 -0500

    Update key.tf

--- jenkins-packer-demo/key.tf
+++ jenkins-packer-demo/key.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "terraformerkeypair" {
+resource "aws_key_pair" "mykeypair" {
   key_name   = "terraformerkeypair"
   public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
   lifecycle {
